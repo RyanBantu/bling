a94c2eac0eb4349bdd81606324a20b259b34604b
Ryan Bantu <ryanbantu@Ryans-MacBook-Pro.local>
2026-02-24 23:57:16 +0530

    Add 4D page with only torus animation visible - no navbar or content

--- src/pages/FourD.js
+++ src/pages/FourD.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const FourD = () => {
   return (
-    <div className="relative z-10 min-h-screen">
-      {/* Empty page - only torus animation visible in background */}
+    <div className="relative z-10 min-h-screen w-full">
+      {/* This page only shows the torus animation - no other content */}
     </div>
   );
 };
