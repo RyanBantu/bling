e7bc1011f1512adf8cffec7ef979624151d7e9de
Ryan Bantu <ryanbantu@Ryans-MacBook-Pro.local>
2026-02-24 20:05:09 +0530

    Change Gallery alt text to avoid ESLint warning

--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -23,7 +23,7 @@ const Gallery = () => {
             >
               <img
                 src={img}
-                alt={`Gallery ${index + 1}`}
+                alt={`Memory ${index + 1}`}
                 className="w-full h-auto object-cover"
               />
             </div>
